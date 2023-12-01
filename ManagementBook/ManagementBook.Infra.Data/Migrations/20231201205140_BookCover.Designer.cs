284037d94e974a700acf373a12656ad0deda6b5d
Aleff Moura <aleff_moura@hotmail.com>
2023-12-01 18:04:35 -0300

    chore: migration for book cover

--- ManagementBook/ManagementBook.Infra.Data/Migrations/20231201205140_BookCover.Designer.cs
+++ ManagementBook/ManagementBook.Infra.Data/Migrations/20231201205140_BookCover.Designer.cs
@@ -4,6 +4,7 @@ using ManagementBook.Infra.Data.Base;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ManagementBook.Infra.Data.Migrations
 {
     [DbContext(typeof(BookStoreContext))]
-    partial class BookStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20231201205140_BookCover")]
+    partial class BookCover
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ManagementBook/ManagementBook.Infra.Data/Migrations/20231201205140_BookCover.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ManagementBook.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookCover : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BookCoverUrl",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookCoverUrl",
+                table: "Books");
+        }
+    }
+}
