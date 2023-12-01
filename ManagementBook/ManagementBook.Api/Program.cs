37b4a072928357614564df2c8b3c8e1d5f80bfbf
Aleff Moura <aleff_moura@hotmail.com>
2023-12-01 01:18:29 -0300

    feat: changed DI for Autofac

--- ManagementBook/ManagementBook.Api/Program.cs
+++ ManagementBook/ManagementBook.Api/Program.cs
@@ -1,17 +1,42 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using ManagementBook.Api.Endpoints;
-using ManagementBook.Api.Extensions;
 using ManagementBook.Api.Handlers;
+using ManagementBook.Api.Modules;
+using MediatR.Extensions.Autofac.DependencyInjection;
+using MediatR.Extensions.Autofac.DependencyInjection.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMvc(options => options.Filters.Add(new ErrorHandlerAttribute()));
+
 builder.Services.Configure<RequestLocalizationOptions>(op =>
 {
     op.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("pt-BR");
 });
-builder.Services.AddDependencies(builder.Configuration);
+
+builder.Host
+       .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+       .ConfigureContainer<ContainerBuilder>(containerBuilder =>
+       {
+           var configuration = MediatRConfigurationBuilder
+                               .Create(typeof(Program).Assembly)
+                               .WithAllOpenGenericHandlerTypesRegistered()
+                               .WithRegistrationScope(RegistrationScope.Scoped)
+                               .Build();
+
+           containerBuilder.RegisterModule(new FluentValidationModule());
+           containerBuilder.RegisterModule(new GlobalModule<Program>(builder.Configuration));
+           containerBuilder.RegisterModule(new MediatRModule());
+
+           containerBuilder.RegisterMediatR(configuration);
+       })
+    .ConfigureHostOptions(o =>
+    {
+        o.ShutdownTimeout = TimeSpan.FromSeconds(60);
+    });
 
 var app = builder.Build();
 
--- ManagementBook/ManagementBook.Application/Features/Books/Commands/BookSaveCommand.cs
+++ ManagementBook/ManagementBook.Application/Features/Books/Commands/BookSaveCommand.cs
@@ -1,5 +1,7 @@
 ﻿namespace ManagementBook.Application.Features.Books.Commands;
 
+using FluentValidation;
+using FluentValidation.Results;
 using LanguageExt.Common;
 using MediatR;
 using System;
@@ -11,4 +13,29 @@ public class BookSaveCommand : IRequest<Result<Unit>>
     public string? Author { get; set; }
     public string? Title { get; set; }
     public DateTime Released { get; set; }
+
+    public ValidationResult Validate()
+        => new BookSaveCommandValidator().Validate(this);
+
+    private class BookSaveCommandValidator : AbstractValidator<BookSaveCommand>
+    {
+        public BookSaveCommandValidator()
+        {
+            RuleFor(a => a.Author)
+                    .NotEmpty()
+                    .WithMessage("Author cant be null or empty")
+                    .Must(x => x is not null && x.Length > 2)
+                    .WithMessage("Author is less than 2.");
+
+            RuleFor(a => a.Title)
+                    .NotEmpty()
+                    .WithMessage("Title cant be null or empty")
+                    .Must(x => x is not null && x.Length > 2)
+                    .WithMessage("Title is less than 2.");
+
+            RuleFor(a => a.Released)
+                    .Must( x => x > DateTime.MinValue)
+                    .WithMessage("Release date is invalid");
+        }
+    }
 }
